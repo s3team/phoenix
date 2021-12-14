7628a7f660a5ffcf54f7b3bf8e38dbb4219b4d33
Dinghao Wu <dwu@ist.psu.edu>
2021-12-13 20:58:24 -0500

    fixed the demo script.

--- demo/demo.sh
+++ demo/demo.sh
@@ -5,14 +5,14 @@
 # works with the directory structure inside
 # the default docker
 
-workdir = $(pwd)
+workdir=$(pwd)
 gcc -m32 -w -no-pie $1 -o $1.exe
-cd /phoenix/uroboros/src
-python urobors.py $workdir/$1.exe
-mv final.s $workdir/$1.exe.s
-cd /phoenix/a2c/src
-python3 a2c.py $workdir/$1.exe.s
-mv output.c $workdir/$1.exe.s.c
+cd ~/phoenix/uroboros/src
+python uroboros.py "$workdir/$1.exe"
+cp final.s "$workdir/$1.exe.s"
+cd ~/phoenix/a2c/src
+python3 a2c.py "$workdir/$1.exe.s"
+cp output.c "$workdir/$1.exe.s.c"
 cd $workdir
-gcc -m32 -w -no-pie foo_p.c -o $1.exe.s.c.exe
+gcc -m32 -w -no-pie $1.exe.s.c -o $1.exe.s.c.exe
 ./$1.exe.s.c.exe
